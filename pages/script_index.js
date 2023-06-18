fbdb6a410cae37e797624a9132f8c047931eaf5b
Rodrigo C <digo.gcardoso@gmail.com>
2023-06-18 16:10:56 -0300

    Adding the task system

--- pages/script_index.js
+++ pages/script_index.js
@@ -1,20 +1,52 @@
 function addTask() {
-    let input = document.getElementById("taskInput");
-    let task = input.value;
-    if (task !== "") {
-      let li = document.createElement("li");
-      li.appendChild(document.createTextNode(task));
-      document.getElementById("taskList").appendChild(li);
-      input.value = "";
-      addDeleteButton(li);
+    var nameInput = document.getElementById("taskName");
+    var descriptionInput = document.getElementById("taskDescription");
+    var weightInput = document.getElementById("taskWeight");
+    var timeInput = document.getElementById("taskTime");
+
+    var name = nameInput.value.trim();
+    var description = descriptionInput.value.trim();
+    var weight = weightInput.value.trim();
+    var time = timeInput.value.trim();
+
+    if (name === '') {
+      alert("Digite o nome da tarefa.");
+      return;
     }
+
+    var task = {
+      name: name,
+      description: description,
+      weight: weight,
+      time: time
+    };
+
+    var taskItem = createTaskItem(task);
+    document.getElementById("taskList").appendChild(taskItem);
+
+    nameInput.value = '';
+    descriptionInput.value = '';
+    weightInput.value = '';
+    timeInput.value = '';
   }
 
-  function addDeleteButton(taskItem) {
-    let deleteButton = document.createElement("button");
+  function createTaskItem(task) {
+    var li = document.createElement("li");
+
+    var taskInfo = document.createElement("div");
+    taskInfo.innerHTML = "<h3>" + task.name + "</h3>" +
+                         "<p>" + task.description + "</p>" +
+                         "<p>Peso: " + task.weight + "</p>" +
+                         "<p>Tempo: " + task.time + "</p>";
+
+    var deleteButton = document.createElement("button");
     deleteButton.appendChild(document.createTextNode("Excluir"));
     deleteButton.onclick = function() {
-      taskItem.parentNode.removeChild(taskItem);
+      li.parentNode.removeChild(li);
     };
-    taskItem.appendChild(deleteButton);
+
+    li.appendChild(taskInfo);
+    li.appendChild(deleteButton);
+
+    return li;
   }
